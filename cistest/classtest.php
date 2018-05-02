18691f496d8edd782b2d58f5f4cbdb44dead668b
CHOIINSEOK_WIN <cis@novowave.co.kr>
2018-05-02 10:04:47 +0900

    cistest 폴더 원격제거

--- cistest/classtest.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	include 'cisinit.php';
- ?>
-
-	<frameset cols="250,500,* ">
-		<frame name="menu" id="menu" src="menu.php" />
-		<frame name="acts" id="acts" src="about:blank" />
-		<frame name="okbits" id="okbits"src="about:blank" />
