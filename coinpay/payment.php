376b69e2c14e5542a0f5f61b266ff6e558f71858
CHOIINSEOK_WIN <cis@novowave.co.kr>
2018-07-02 13:40:06 +0900

    no message

--- coinpay/payment.php
+++ coinpay/payment.php
@@ -56,10 +56,9 @@ $otpCode  = $pay_otp;
 
         $endpoint =$_SESSION['receive_check_url'];
         
-        $msg = '결제금액에 대한 정보가 변경되어 결제처리할수 없습니다. 다시 금액 확인후 주문해주세요';
-		return ajaxFail($msg);
+        
 
-    	/*$resultchk_curl = curl_init();
+    	$resultchk_curl = curl_init();
 		curl_setopt($resultchk_curl, CURLOPT_URL, $endpoint);						
 		curl_setopt($resultchk_curl, CURLOPT_RETURNTRANSFER,true);
 		if (strpos($endpoint,"https") !== false ){
@@ -72,7 +71,12 @@ $otpCode  = $pay_otp;
 		$response = curl_exec($resultchk_curl);
 		curl_close($resultchk_curl);
 		//var_dump($response);
-
+		
+		
+		
+		$msg = '결제금액에 대한 정보가 변경되어 결제처리할수 없습니다. 다시 금액 확인후 주문해주세요';
+		return ajaxFail($msg);
+		/*
 		$arrayRes = (array) json_decode($response);
 	
 		if ($arrayRes["resstatus"] !== "0000") {
